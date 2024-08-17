d6faff225460e290f4ac7b9f66d92744b9fa4dc5
srinivasuGit <srinivasu1243@gmail.com>
2024-08-17 15:20:58 +0530

    New Method Created

--- src/main/java/com/example/gitdemo/controller/HelloWorldController.java
+++ src/main/java/com/example/gitdemo/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.example.gitdemo.controller;
 
+import jakarta.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class HelloWorldController {
     @GetMapping
     public String sayHello(){
         return "Hello Welcome !!!";
+    }
+
+    @GetMapping("/{name}")
+    public String welcomeMessage(@PathParam("name") String name){
+        return "Welcome" +name;
     }
 }
